2c66c4d133c6998a03b6ef92a5635ea7627c30fe
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-02 12:53:06 -0600

    Splash Screen Implentation

---
+++ BufetecAppPrototipo/Main/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/2/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ BufetecAppPrototipo/Main/SplashScreenApp.swift
@@ -0,0 +1,8 @@
+//
+//  SplashScreenApp.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/2/24.
+//
+
+import Foundation

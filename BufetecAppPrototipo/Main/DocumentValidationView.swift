002b56c9c85cd8eefd254adeeddd8708195b612f
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-05 00:25:15 -0600

    DocumentValidationView implementation

---
+++ BufetecAppPrototipo/Main/DocumentValidationView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentValidationView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/4/24.
+//
+
+import SwiftUI
+
+struct DocumentValidationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentValidationView()
+}

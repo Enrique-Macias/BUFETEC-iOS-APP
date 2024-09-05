c0f7fd93166f2ef0231b4a0dc21e27df3716bfe6
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-05 13:51:09 -0600

    Update

--- BufetecAppPrototipo/Main/ChatBotViews/LoadView.swift
+++ BufetecAppPrototipo/Main/ChatBotViews/LoadView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct LoadView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color(colorScheme == .light ? Color("btBlue") : .white)
+                .edgesIgnoringSafeArea(.all)
+            Image("ChatAILogo")
+                .foregroundColor(Color(colorScheme == .light ? .white : Color("btBlue")))
+            
+        }
+    }
+}
+
+struct LoadView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LoadView()
+                .preferredColorScheme(.light) // Modo claro
+            LoadView()
+                .preferredColorScheme(.dark)  // Modo oscuro
+        }
     }
 }
 

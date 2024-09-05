1f4db31a5defc0941377b19d46519f3ceda272b7
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-05 01:24:49 -0600

    Scan Document Implementation

---
+++ BufetecAppPrototipo/Main/DocumentScanView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentScanView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/5/24.
+//
+
+import SwiftUI
+
+struct DocumentScanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentScanView()
+}

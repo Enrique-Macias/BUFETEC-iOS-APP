b90dc37263ceb461c0bdc34f2fbbf6cb109a25df
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-06 01:58:29 -0600

    OnboardingScreen Implementation

---
+++ BufetecAppPrototipo/Main/OnboardingViews/OnboardingScreen1View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingScreen1View.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/6/24.
+//
+
+import SwiftUI
+
+struct OnboardingScreen1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingScreen1View()
+}
---
+++ BufetecAppPrototipo/Main/OnboardingViews/OnboardingScreen3View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingScreen3View.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/6/24.
+//
+
+import SwiftUI
+
+struct OnboardingScreen3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingScreen3View()
+}

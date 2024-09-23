72920947d3ee6d38fabd25911ce112dbdfae8930
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-23 13:46:09 -0600

    Cambia nombre de API key y  Generative plist a Secrets.plist

--- BufetecAppPrototipo/Main/ChatBotViews/APIKey.swift
+++ BufetecAppPrototipo/Main/ChatBotViews/APIKey.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 enum APIKey {
-    // Fetch the API key from 'GenerativeAI-Info.plist'
+    // Fetch the API key from 'Secrets.plist'
     static var `default`: String {
-        guard let filePath = Bundle.main.path(forResource: "GenerativeAI-Info", ofType: "plist")
+        guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist")
         else {
-            fatalError("Couldn't find file 'GenerativeAI-Info.plist'.")
+            fatalError("Couldn't find file 'Secrets.plist'.")
         }
         let plist = NSDictionary(contentsOfFile: filePath)
-        guard let value = plist?.object(forKey: "API_KEY") as? String else {
-            fatalError("Couldn't find key 'API_KEY' in 'GenerativeAI-Info.plist'.")
+        guard let value = plist?.object(forKey: "GM_API_KEY") as? String else {
+            fatalError("Couldn't find key 'GM_API_KEY' in 'Secrets.plist'.")
         }
         if value.starts(with: "_") {
             fatalError("Follow the instructions at https://ai.google.dev/tutorials/setup to get an API key."

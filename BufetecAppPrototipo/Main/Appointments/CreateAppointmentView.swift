b03385915ce36a281a2792bf7d020b48cdbe5eca
Enrique Macias Lopez <a01641402@tec.mx>
2024-09-24 12:45:27 -0600

    CreateAppointmetView

---
+++ BufetecAppPrototipo/Main/Appointments/CreateAppointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAppointmentView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Enrique Macias on 9/24/24.
+//
+
+import SwiftUI
+
+struct CreateAppointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAppointmentView()
+}

42854bc8c668d17c531c060857d24ed9ca70e6eb
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-29 17:19:07 -0600

    Agrega splashscreen, ahora login view y sign up view se ven mas consistentes.

---
+++ BufetecAppPrototipo/Main/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Ramiro Uziel Rodriguez Pineda on 29/09/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}

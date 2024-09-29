249816857653b8e9386e2e6981b914f880028758
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-29 17:19:11 -0600

    Agrega splashscreen, ahora login view y sign up view se ven mas consistentes.

--- BufetecAppPrototipo/Main/BufetecApp.swift
+++ BufetecAppPrototipo/Main/BufetecApp.swift
@@ -1,16 +1,22 @@
 import SwiftUI
 
+class SplashScreenState: ObservableObject {
+    @Published var isFinished = false
+    @Published var logoPosition: CGPoint = .zero
+}
+
 @main
 struct BufetecApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var authModel = AuthModel()
+    @StateObject private var splashScreenState = SplashScreenState()
     @State var appearanceManager = AppearanceManager()
-    @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(authModel)
+                .environmentObject(splashScreenState)
                 .environment(appearanceManager)
                 .onAppear {
                     appearanceManager.initAppearanceStyle()
@@ -21,12 +27,30 @@ struct BufetecApp: App {
 
 struct ContentView: View {
     @AppStorage("hasSeenOnboarding") var hasSeenOnboarding: Bool = false
+    @EnvironmentObject var splashScreenState: SplashScreenState
     
     var body: some View {
-        if hasSeenOnboarding {
-            AuthenticationView()
-        } else {
-            WelcomeScreenView()
+        ZStack {
+            Group {
+                if hasSeenOnboarding {
+                    AuthenticationView()
+                } else {
+                    WelcomeScreenView()
+                }
+            }
+            .opacity(splashScreenState.isFinished ? 1 : 0)
+            .animation(.easeIn(duration: 0.3), value: splashScreenState.isFinished)
+            
+            if !splashScreenState.isFinished {
+                SplashScreenView()
+            }
         }
     }
+}
+
+#Preview {
+    ContentView()
+        .environmentObject(AuthModel())
+        .environmentObject(SplashScreenState())
+        .environment(AppearanceManager())
 }

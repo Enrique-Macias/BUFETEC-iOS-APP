6163c67d8c504f2765a5dadc4cf5c76dc43285f6
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-30 12:22:56 -0600

    Cambios en AppointmentView

--- BufetecAppPrototipo/Main/SplashScreenView.swift
+++ BufetecAppPrototipo/Main/SplashScreenView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @EnvironmentObject var authModel: AuthModel
     @EnvironmentObject var appState: AppState
     
     var body: some View {
@@ -14,17 +15,26 @@ struct SplashScreenView: View {
                 .position(appState.logoPosition)
         }
         .onAppear {
-            // Add a delay before changing the position
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                withAnimation(.spring(duration: 0.8)) {
-                    appState.logoPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height * 0.1)
+            if authModel.authState != .authenticated {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    withAnimation(.spring(duration: 0.8)) {
+                        appState.logoPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height * 0.1)
+                    }
                 }
+            } else if authModel.authState != .signedOut {
+                appState.logoPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height * 0.1)
             }
             
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation(.easeInOut(duration: 0.5)) {
-                    appState.isShowingSplash = false
+            if authModel.authState != .authenticated {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        appState.isShowingSplash = false
+                    }
                 }
+            }
+            else {
+                appState.isShowingSplash = false
+                
             }
         }
     }

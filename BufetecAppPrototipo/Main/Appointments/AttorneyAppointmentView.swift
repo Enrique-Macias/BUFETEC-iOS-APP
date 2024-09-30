4add40557d7c25756f4f9a04454f10d78eb74629
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-30 14:53:25 -0600

    Crea vista para selección de horarios para abogado.

---
+++ BufetecAppPrototipo/Main/Appointments/AttorneyAppointmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AttorneyAppointmentView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Ramiro Uziel Rodriguez Pineda on 30/09/24.
+//
+
+import SwiftUI
+
+struct AttorneyAppointmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttorneyAppointmentView()
+}

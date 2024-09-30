1e3eb2786f28408fe6189d89b34955bf054fe3e5
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-29 19:09:27 -0600

    Separa additional info view.

---
+++ BufetecAppPrototipo/Main/Auth/AdditionalInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionalInfoView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Ramiro Uziel Rodriguez Pineda on 29/09/24.
+//
+
+import SwiftUI
+
+struct AdditionalInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionalInfoView()
+}

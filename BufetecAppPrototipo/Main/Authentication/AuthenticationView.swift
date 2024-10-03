c3f2880980eb1f19f0aaf613a5027f16e6fcecd3
ramiro-uziel <ramirouziel15@gmail.com>
2024-10-02 19:31:32 -0600

    Arregla nav titles no apareciendo despues de login / registro.

--- BufetecAppPrototipo/Main/Authentication/AuthenticationView.swift
+++ BufetecAppPrototipo/Main/Authentication/AuthenticationView.swift
@@ -7,39 +7,37 @@ struct AuthenticationView: View {
     @State private var isShowingSignUp = false
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                VStack {
-                    switch authModel.authState {
-                    case .signedOut, .signingIn:
-                        if isShowingSignUp {
-                            SignUpView(isShowingSignUp: $isShowingSignUp)
-                        } else {
-                            LoginView(isShowingSignUp: $isShowingSignUp)
-                        }
-                    case .needsAdditionalInfo:
-                        AdditionalInfoView()
-                    case .authenticating:
-                        ProgressView("Completando perfil...")
-                    case .authenticated:
-                        CustomTabView()
-                    case .needsEmailVerification:
-                        EmailVerificationView()
+        ZStack {
+            VStack {
+                switch authModel.authState {
+                case .signedOut, .signingIn:
+                    if isShowingSignUp {
+                        SignUpView(isShowingSignUp: $isShowingSignUp)
+                    } else {
+                        LoginView(isShowingSignUp: $isShowingSignUp)
                     }
-                }
-                .disabled(authModel.isLoading)
-                
-                if authModel.isLoading {
-                    ProgressView()
-                        .scaleEffect(1.5)
-                        .padding()
-                        .background(Color.white.opacity(0.8))
-                        .cornerRadius(10)
+                case .needsAdditionalInfo:
+                    AdditionalInfoView()
+                case .authenticating:
+                    ProgressView("Completando perfil...")
+                case .authenticated:
+                    CustomTabView()
+                case .needsEmailVerification:
+                    EmailVerificationView()
                 }
             }
-            .onAppear {
-                authModel.checkUserSession()
+            .disabled(authModel.isLoading)
+            
+            if authModel.isLoading {
+                ProgressView()
+                    .scaleEffect(1.5)
+                    .padding()
+                    .background(Color.white.opacity(0.8))
+                    .cornerRadius(10)
             }
+        }
+        .onAppear {
+            authModel.checkUserSession()
         }
     }
 }

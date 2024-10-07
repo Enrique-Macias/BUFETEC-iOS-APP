f749682715d2d6e38d71db282bd7c6f3d2fda111
mpgzz <a00837313@tec.mx>
2024-10-07 11:20:00 -0600

    lawyer view appointments

--- BufetecAppPrototipo/Main/ClientProfileView.swift
+++ BufetecAppPrototipo/Main/ClientProfileView.swift
@@ -1,18 +1,18 @@
 //
-//  AppointmentsClientView.swift
+//  ClientProfileView.swift
 //  BufetecAppPrototipo
 //
-//  Created by Μιτχελλ on 30/09/24.
+//  Created by Μιτχελλ on 03/10/24.
 //
 
 import SwiftUI
 
-struct AppointmentsClientView: View {
+struct ClientProfileView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AppointmentsClientView()
+    ClientProfileView()
 }
--- BufetecAppPrototipo/Main/ClientsView.swift
+++ BufetecAppPrototipo/Main/ClientsView.swift
@@ -1,18 +1,18 @@
 //
-//  AppointmentsClientView.swift
+//  ClientsView.swift
 //  BufetecAppPrototipo
 //
-//  Created by Μιτχελλ on 30/09/24.
+//  Created by Μιτχελλ on 03/10/24.
 //
 
 import SwiftUI
 
-struct AppointmentsClientView: View {
+struct ClientsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AppointmentsClientView()
+    ClientsView()
 }

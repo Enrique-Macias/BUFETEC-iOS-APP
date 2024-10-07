99755f9b3825a506ea4864a77ed4e634a680787b
mpgzz <a00837313@tec.mx>
2024-10-07 12:11:59 -0600

    comit

--- BufetecAppPrototipo/Main/Appointments/AppointmentsClientView.swift
+++ BufetecAppPrototipo/Main/Appointments/AppointmentsClientView.swift
@@ -1,18 +1,15 @@
-//
-//  AppointmentsClientView.swift
-//  BufetecAppPrototipo
-//
-//  Created by Μιτχελλ on 30/09/24.
-//
-
 import SwiftUI
 
 struct AppointmentsClientView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack(){
+            
+        }
     }
 }
 
 #Preview {
     AppointmentsClientView()
+        .environment(AppearanceManager())
 }
+

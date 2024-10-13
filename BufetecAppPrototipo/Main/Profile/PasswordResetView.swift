203ddfc0269ab09e29bdcdea9d3b4ba682eb6886
ramiro-uziel <ramirouziel15@gmail.com>
2024-10-12 23:25:41 -0600

    Cambios de UI en ClientView y agrega funcionalidad para reestablecer password.

---
+++ BufetecAppPrototipo/Main/Profile/PasswordResetView.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordResetView.swift
+//  BufetecAppPrototipo
+//
+//  Created by Ramiro Uziel Rodriguez Pineda on 12/10/24.
+//
+
+import SwiftUI
+
+struct PasswordResetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordResetView()
+}
